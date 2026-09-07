e94ea37def99e59e0d5f26afc0129f50220354ac
Velin-walk <walknepalwalk@gmail.com>
2026-09-07 21:43:41 +0545

    Add MapViewer component for displaying geo data

---
+++ components/MapViewer.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { kml, gpx } from "@tmcw/togeojson";
+import L from 'leaflet';
+
+function ZoomToData({ data }: { data: any }) {
+  const map = useMap();
+  useEffect(() => {
+    if (data) {
+      const layer = L.geoJSON(data);
+      map.fitBounds(layer.getBounds());
+    }
+  }, [data, map]);
+  return null;
+}
+
+export default function MapViewer({ url, type }: { url: string, type: string }) {
+  const [geoData, setGeoData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch(url).then(r => r.text()).then(text => {
+      const dom = new DOMParser().parseFromString(text, "text/xml");
+      const converted = type === 'gpx' ? gpx(dom) : type === 'kml' ? kml(dom) : JSON.parse(text);
+      setGeoData(converted);
+    });
+  }, [url, type]);
+
+  return (
+    <div className="h-[500px] w-full rounded-lg overflow-hidden border">
+      <MapContainer center={[0, 0]} zoom={2} className="h-full w-full">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {geoData && <><GeoJSON data={geoData} /><ZoomToData data={geoData} /></>}
+      </MapContainer>
+    </div>
+  );
+}
